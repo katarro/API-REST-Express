f95d555bd3fea81a111048622b1e74ac9a281758
felipe <pipe.castro.espinoza@gmail.com>
2021-11-24 19:09:52 -0600

    finished

--- store/index.js
+++ store/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const routerApi = require('./routes/index');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 const {
   logErrors,
