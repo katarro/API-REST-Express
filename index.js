481801f3b53d576153fb1b466a2aa8a3d438b787
Felipe Castro <66927047+katarro@users.noreply.github.com>
2021-11-28 14:34:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(errorHandler);
 app.get('/', (req, res) => {
   let message = 'Hola, este es mi server en Express.';
   let message2 = 'Estas son las rutas de la api en las que puedes acceder: ';
-  let message3 = ' -- /api/categories';
-  let message4 = ' -- /api/users';
-  let message5 = ' -- /api/products';
+  let message3 = ' -- /api/v1/categories';
+  let message4 = ' -- /api/v1/users';
+  let message5 = ' -- /api/v1/products';
   res.send(message + message2 + message3 + message4 + message5);
 });
 
